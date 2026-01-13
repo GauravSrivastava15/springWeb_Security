8600fbcb7603f1c1d568f76290ca7b71a36617f6
Gaurav.S.Srivastava <gauravsrivastava1501@gmail.com>
2026-01-13 21:10:27 +0530

    Session management with JWT

---
+++ src/main/java/com/SecurityApp/SecurityApplication/entity/Session.java
@@ -0,0 +1,29 @@
+package com.SecurityApp.SecurityApplication.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+public class Session {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String refreshToken;
+
+
+    private LocalDateTime lastUsedAt;
+
+    @ManyToOne
+    private User user;
+}
---
+++ src/main/java/com/SecurityApp/SecurityApplication/repositories/SessionRepository.java
@@ -0,0 +1,14 @@
+package com.SecurityApp.SecurityApplication.repositories;
+
+import com.SecurityApp.SecurityApplication.entity.Session;
+import com.SecurityApp.SecurityApplication.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SessionRepository extends JpaRepository<Session, Long> {
+    List<Session> findByUser(User user);
+
+    Optional<Session> findByRefreshToken(String refreshToken);
+}
